0b17734b2ae4ed2e967ee1703724d4cdf6a612a1
Mike Ackerman <dirtpathwanderer@gmail.com>
2025-02-14 10:42:23 -0700

    started leaflet w pops and ma/mi spawn areas

--- scripts/sr_avai_hab_leaflet.R
+++ scripts/sr_avai_hab_leaflet.R
@@ -21,7 +21,6 @@ library(leaflet)
 
 # -----------------------
 # compile data
-
 # populations
 load(here("data/spatial/SR_pops.rda")) ; rm(fall_pop)
 sthd_pops = sth_pop %>%
@@ -39,3 +38,75 @@ chnk_spawn = readRDS(here("data/spatial/spsm_spwn_areas.rds")) %>%
 load(file = here("data/spatial/prepped_snake_ip.rda"))
 qrf_sf = get(load(file = here("data/spatial/snake_redd_qrf.rda")))
 
+# -----------------------
+# set some colors
+sthd_mpg_col = colorFactor(palette = "Dark2", domain = sthd_pops$MPG)
+sthd_spawn_col = colorFactor(palette = c("skyblue", "navy"), domain = sthd_spawn$TYPE, reverse = TRUE)
+chnk_mpg_col = colorFactor(palette = "Set1", domain = chnk_pops$MPG)
+chnk_spawn_col = colorFactor(palette = c("springgreen", "darkgreen"), domain = chnk_spawn$TYPE, reverse = TRUE)
+
+# -----------------------
+# build leaflet
+base = leaflet() %>%
+  # base map
+  setView(lng = -116, lat = 45.35, zoom = 7.5) %>%
+  addProviderTiles(providers$Esri.WorldTopoMap)
+
+sr_hab_leaflet = base %>%
+  # steelhead populations
+  addPolygons(data = sthd_pops,
+              group = "Steelhead Populations",
+              fillColor = ~sthd_mpg_col(MPG),
+              fillOpacity = 0.2,
+              stroke = T,
+              weight = 2,
+              color = "black",
+              opacity = 1,
+              label = ~paste0(TRT_POPID, ": ", POP_NAME),
+              popup = paste("<b>Steelhead</b></br>",
+                            "<b>Pop ID:</b>", sthd_pops$TRT_POPID, "</br>",
+                            "<b>Pop Name:</b>", sthd_pops$POP_NAME, "</br>",
+                            "<b>MPG:</b>", sthd_pops$MPG, "</br>")) %>%
+  # steelhead major/minor spawning areas
+  addPolygons(data = sthd_spawn,
+              group = "Steelhead Spawning Areas",
+              fillColor = ~sthd_spawn_col(TYPE),
+              fillOpacity = 0.2,
+              stroke = T,
+              weight = 1,
+              color = "black",
+              opacity = 1,
+              label = ~paste0(POP_NAME, ": ", MSA_NAME, ", ", TYPE)) %>%
+  # chinook salmon populations
+  addPolygons(data = chnk_pops,
+              group = "Sp/Sum Chinook Populations",
+              fillColor = ~chnk_mpg_col(MPG),
+              fillOpacity = 0.2,
+              stroke = T,
+              weight = 2,
+              color = "black",
+              opacity = 1,
+              label = ~paste0(TRT_POPID, ": ", POP_NAME),
+              popup = paste("<b>sp/sum Chinook salmon</b></br>",
+                            "<b>Pop ID:</b>", chnk_pops$TRT_POPID, "</br>",
+                            "<b>Pop Name:</b>", chnk_pops$POP_NAME, "</br>",
+                            "<b>MPG:</b>", chnk_pops$MPG, "</br>")) %>%
+  # chinook major/minor spawning areas
+  addPolygons(data = chnk_spawn,
+              group = "Sp/Sum Chinook Spawning Areas",
+              fillColor = ~chnk_spawn_col(TYPE),
+              fillOpacity = 0.2,
+              stroke = T,
+              weight = 1,
+              color = "black",
+              opacity = 1,
+              label = ~paste0(POP_NAME, ": ", MSA_NAME, ", ", TYPE)) %>%
+  addLayersControl(baseGroups = c("Steelhead Populations",
+                                  "Steelhead Spawning Areas",
+                                  "Sp/Sum Chinook Populations",
+                                  "Sp/Sum Chinook Spawning Areas"),
+                   options = layersControlOptions(collapsed = FALSE))
+
+sr_hab_leaflet
+  
+  
