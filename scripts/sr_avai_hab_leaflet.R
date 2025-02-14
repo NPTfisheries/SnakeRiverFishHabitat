670e65a87ac57c08364e33fa4862e3be2917f7a5
Mike Ackerman <dirtpathwanderer@gmail.com>
2025-02-14 10:28:09 -0700

    compiled data in script

--- scripts/sr_avai_hab_leaflet.R
+++ scripts/sr_avai_hab_leaflet.R
@@ -28,3 +28,14 @@ sthd_pops = sth_pop %>%
   select(TRT_POPID, POP_NAME, MPG) ; rm(sth_pop)
 chnk_pops = spsm_pop %>%
   select(TRT_POPID, POP_NAME, MPG) ; rm(spsm_pop)
+
+# major/minor spawning areas
+sthd_spawn = load(here("data/spatial/steelhead_gis_data.rda")) %>%
+  {sthd_spawn %>% st_transform("EPSG:4326")} ; rm(sthd_critical, sthd_extant, sthd_ip, sthd_huc)
+chnk_spawn = readRDS(here("data/spatial/spsm_spwn_areas.rds")) %>%
+  st_transform("EPSG:4326")
+
+# prepped intrinsic potential and redd qrf datasets
+load(file = here("data/spatial/prepped_snake_ip.rda"))
+qrf_sf = get(load(file = here("data/spatial/snake_redd_qrf.rda")))
+
